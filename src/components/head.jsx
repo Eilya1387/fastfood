b761ca1fb4adc19be6d1cb779fcadae09ef00d98
Eilya.master <Eilyamirmozafary@gmail.com>
2025-06-05 15:53:11 +0330

    head risponsive

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -12,16 +12,6 @@ const Head = () => {
             <h1>فست فود رستوران</h1>
             <h1>مک دونالد</h1>
           </div>
-          {/* <div className="offer">
-            <div className="circle">
-              <span>20%</span>
-              <span>off</span>
-            </div>
-            <h3>
-              در اولین سفارش خود، اگر به صورت آنلاین از طریق برنامه ما سفارش
-              دهید!
-            </h3>
-          </div> */}
         </div>
       </header>
     </>
